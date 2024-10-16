0e4d7fa383e11a8e57dab754d9e025311af4474d
Manas Raj <manasraj1357@gmail.com>
2024-10-16 12:17:23 +0530

    Completed Frontend portion of Login, Signup and Reset Password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landingpage from "./pages/Landingpage"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Resetpassword from "./pages/Resetpassword"
+
 
 function App() {
   return (
         <Router>
           <Routes>
             <Route path="/" element={<Landingpage />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Signup" element={<Signup />} />
+            <Route path="/Resetpassword" element={<Resetpassword />} />
           </Routes>
         </Router>
   );
