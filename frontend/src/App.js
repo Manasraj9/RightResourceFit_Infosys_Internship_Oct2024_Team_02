c94481dd867d737483bdc7147a84d3a5061d9e53
priyankas1907 <priyankasalethmary@gmail.com>
2024-10-17 11:43:16 +0530

    -

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,8 @@ import Landingpage from "./pages/Landingpage"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Resetpassword from "./pages/Resetpassword"
-<<<<<<< HEAD
 import Homepage from './pages/Homepage'
-=======
 import OtpVerification from  "./pages/OtpVerification"
->>>>>>> origin/main
-
 
 function App() {
   return (
