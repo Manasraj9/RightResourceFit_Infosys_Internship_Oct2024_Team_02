3887046929ae42418a39cab0c3e99881d12026ca
Manas Raj <manasraj1357@gmail.com>
2024-10-25 18:02:51 +0530

    Connected the pages

--- frontend/src/pages/Companypage.jsx
+++ frontend/src/pages/Companypage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../Components/Navbar';
+import Navbar from '../Components/NavbarCompany';
 import Footer from '../Components/Footer';
 import Sidebar from '../Components/side__bar.jsx';
 import SecondaryNavbar from '../Components/Sec_Navbar.jsx';
